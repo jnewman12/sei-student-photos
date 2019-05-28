586e818a460b410d05c46eb82be841d544bc820a
James Newman <jamesmnewman1@gmail.com>
2019-05-27 17:19:20 -0700

    adding to project

---
+++ server.js
@@ -0,0 +1,98 @@
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+// var app = express();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
+
+
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+// session middleware
+var session = require('express-session');
+var passport = require('passport');
+var methodOverride = require('method-override');
+
+// load the env vars
+require('dotenv').config();
+
+// create the Express app
+var app = express();
+
+// connect to the MongoDB with mongoose
+require('./config/database');
+// configure Passport
+require('./config/passport');
+
+// require our routes
+var indexRoutes = require('./routes/index');
+var studentsRoutes = require('./routes/students');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+// mount the session middleware
+app.use(session({
+  secret: 'SEI Rocks!',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// mount all routes with appropriate base paths
+app.use('/', indexRoutes);
+app.use('/', studentsRoutes);
+
+// invalid request, send 404 page
+app.use(function(req, res) {
+  res.status(404).send('Cant find that!');
+});
+
+module.exports = app;
